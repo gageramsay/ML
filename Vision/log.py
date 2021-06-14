e3eb4ef04d5f7a1c6d4c5f23646d2ea57e681323
gageramsay <developergage@gmail.com>
2021-06-14 10:59:44 -0600

    labels corrections

--- Vision/log.py
+++ Vision/log.py
@@ -14,9 +14,12 @@ TODO: create more image data and create corresponding labels
 
 """
 # x_midpoint, y_midpoint, width, height
-boxes = [(0.525, 0.5, 0.035, 0.9),
-         (0.47, 0.52, 0.70, 0.05)]
-image_1 = Image.open("./data/BBlox/BBloxImages/1.jpg")
+# boxes = [(0.38, 0.74, 0.28, 0.32),
+#          (0.47, 0.52, 0.70, 0.05)]
+boxes = [(0.42, 0.66, 0.2, 0.27),
+         (0.64, 0.67, 0.22, 0.28),
+         (0.63, 0.37, 0.177, 0.23)    ]
+image_1 = Image.open("./data/BBlox/BBloxImages/3.jpg")
 
 plot_image(image_1, boxes)
 
